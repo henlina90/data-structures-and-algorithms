10a0b3b1f4ed0b2ec0a7001ff29c274cd5df59eb
henlina90 <henlina90>
2021-11-14 20:04:26 -0800

    solution

--- Trees_Assignment_1/src/StarshipEnterprise.js
+++ Trees_Assignment_1/src/StarshipEnterprise.js
@@ -11,20 +11,88 @@ class StarshipEnterprise {
 
   assignOfficer(officerId, officerName) {
     // your solution here
+    if (this.officerId == null) {
+      this.officerId = officerId;
+      this.officerName = officerName;
+    } else if (officerId < this.officerId) {
+      if (this.leftReport == null) {
+        this.leftReport = new StarshipEnterprise(officerId, officerName, this);
+      } else {
+        this.leftReport.assignOfficer(officerId, officerName);
+      }
+    } else {
+      if (this.rightReport == null) {
+        this.rightReport = new StarshipEnterprise(officerId, officerName, this);
+      } else {
+        this.rightReport.assignOfficer(officerId, officerName);
+      }
+    }
   }
 
   findOfficersWithNoDirectReports(values = []) {
     // your solution here
+    if (this.leftReport == null && this.rightReport == null) {
+      values = [this.officerName, ...values];
+    } else if (this.leftReport && this.rightReport) {
+      values = [
+        ...this.leftReport.findOfficersWithNoDirectReports(values),
+        ...this.rightReport.findOfficersWithNoDirectReports(values),
+        ...values,
+      ];
+    } else if (this.leftReport != null) {
+      values = [
+        ...this.leftReport.findOfficersWithNoDirectReports(values),
+        ...values,
+      ];
+    } else if (this.rightReport != null) {
+      values = [
+        ...this.rightReport.findOfficersWithNoDirectReports(values),
+        ...values,
+      ];
+    }
     return values;
   }
 
   listOfficersByExperience(officerNames = []) {
     // your solution here
+    if (this.rightReport) {
+      officerNames = this.rightReport.listOfficersByExperience(officerNames);
+    }
+    officerNames.push(this.officerName);
+    if (this.leftReport) {
+      officerNames = this.leftReport.listOfficersByExperience(officerNames);
+    }
+    // console.log({ officerNames });
     return officerNames;
   }
 
   listOfficersByRank(tree, rankedOfficers = {}) {
     // your solution here
+    const queue = new Queue();
+    let depth = 1;
+    queue.enqueue(tree);
+    let node = queue.dequeue();
+    while (node) {
+      depth = 1;
+      let checker = node.reportTo;
+      while (checker) {
+        depth++;
+        checker = checker.reportTo;
+      }
+      if (!rankedOfficers[depth]) {
+        rankedOfficers[depth] = [];
+      }
+      rankedOfficers[depth].push(node.officerName);
+      if (node.rightReport) {
+        queue.enqueue(node.rightReport);
+      }
+
+      if (node.leftReport) {
+        queue.enqueue(node.leftReport);
+      }
+      node = queue.dequeue();
+    }
+    // console.log({ rankedOfficers });
     return rankedOfficers;
   }
 }
